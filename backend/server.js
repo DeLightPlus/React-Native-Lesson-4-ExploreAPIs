cb0416ce72d06c165a4b9a2dbf438cbecdea6c0b
DeLightPlus <matlakalakabelo1@gmail.com>
2024-12-17 08:46:06 +0200

    Basic UI and navigation to payment screen

--- backend/server.js
+++ backend/server.js
@@ -14,19 +14,58 @@ app.use(express.json());
 
 console.log(process.env.STRIPE_SECRET_KEY);
 
+
+app.post('/payment-sheet', async (req, res) => {
+  const { amount } = req.body;
+  console.log("reached out with amount: R", amount);
+
+  // Use an existing Customer ID if this is a returning customer.
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2024-11-20.acacia'}
+  );
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount * 100,
+    currency: 'zar', 
+    // currency: 'eur', 
+    customer: customer.id,
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+    // is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+    publishableKey: 'pk_test_51QV5eXKfXucQGnUWPUDI6M2raAoebipaveIJLPgZEgNoFgkKk7yRi1lZH7lXYEpoonLNYm1JmwovnwVQsYhnnX5h00fjH0z8uv'
+  });
+});
+
 app.post("/payment-intent", async (req, res) => {
     const { amount } = req.body;
+    console.log(amount);
+    
   
-    try {
+    try 
+    {
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount * 100, // Convert amount to cents
         currency: "zar",
       });
   
       res.json({ clientSecret: paymentIntent.client_secret });
-    } catch (error) {
+    } 
+    catch (error) 
+    {
       res.status(500).json({ error: error.message });
     }
   });
   
-  app.listen(PORT, () => console.log("Server running on port: "+PORT));
+
+
+
+app.listen(PORT, () => console.log("Server running on port: "+PORT));
--- frontend/app/payment.tsx
+++ frontend/app/payment.tsx
@@ -1,16 +1,89 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { useState, useEffect } from "react";
+import { useStripe } from '@stripe/stripe-react-native';
+
 
 export default function PaymentScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Payment Screen</Text>
-      <Text style={styles.text}>Choose your payment method (Stripe, PayPal, Razorpay - Placeholder)</Text>
-      <Button
-        title="Proceed with Payment"
-        onPress={() => alert('Payment would be processed here.')}
-      />
-    </View>
-  );
+    const [amount, setAmount] = useState(1150);
+
+    const { initPaymentSheet, presentPaymentSheet } = useStripe();
+    const [loading, setLoading] = useState(false);
+
+    const API_URL = "http://192.168.18.28:3080";
+
+    const fetchPaymentSheetParams = async () => {
+        const response = await fetch(`${API_URL}/payment-sheet`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ amount: amount })
+        });
+        const { paymentIntent, ephemeralKey, customer } = await response.json();
+    
+        return {
+          paymentIntent,
+          ephemeralKey,
+          customer,
+        };
+    };
+
+    const initializePaymentSheet = async () => {
+        const {
+          paymentIntent,
+          ephemeralKey,
+          customer,
+        } = await fetchPaymentSheetParams();
+    
+        const { error } = await initPaymentSheet({
+                merchantDisplayName: "mLab-CodeTribe-2024/2025",
+                customerId: customer,
+                customerEphemeralKeySecret: ephemeralKey,
+                paymentIntentClientSecret: paymentIntent,
+                // Set `allowsDelayedPaymentMethods` to true if your business can handle payment
+                //methods that complete payment after a delay, like SEPA Debit and Sofort.
+                allowsDelayedPaymentMethods: true,
+                defaultBillingDetails: {
+                name: 'Kabelo Matlakala',
+            }
+        });
+
+        if (!error) { setLoading(true); }
+    };
+    
+    const openPaymentSheet = async () => {
+        const { error } = await presentPaymentSheet();
+    
+        if (error) 
+        {
+          alert(`Error code: ${error.code}`, error.message);
+        } 
+        else 
+        {
+          alert('Success', 'Your order is confirmed!');
+        }
+    };
+    
+    // useEffect(() => {
+    //     initializePaymentSheet();
+    // }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}></Text>
+            <Text style={styles.text}>Choose your payment method (Stripe, PayPal, Razorpay - Placeholder)</Text>
+
+            <Pressable style={styles.payBtn}
+                onPress={ async () => { 
+                    alert("Button Pressed, Please wait!!!")
+                    await initializePaymentSheet()
+                    .then(async ()=> { await openPaymentSheet() })                    
+                }}
+            >
+                <Text>Proceed to Payment</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -30,4 +103,8 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     textAlign: 'center',
   },
+  payBtn:{
+    padding:8,
+    backgroundColor:"wheat"
+  }
 });
